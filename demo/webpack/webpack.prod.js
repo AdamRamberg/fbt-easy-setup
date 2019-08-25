dd600e18e215f5fe46fae68be1c147c44eca8c85
AdamRamberg <adam@mambojambostudios.com>
2019-08-25 21:11:00 +0200

    Prepartion for gh-pages

---
+++ demo/webpack/webpack.prod.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const plugins = [];
+
+module.exports = require('./webpack.base')({
+  mode: 'production',
+  devServer: {
+    port: 3000,
+    contentBase: path.join(process.cwd(), 'dist/'),
+    historyApiFallback: true,
+  },
+  plugins,
+});
